2e0375b3166c4f58e2fc0331451383c908e3f050
Henry Tang <hktang@ualberta.ca>
2019-01-26 23:19:29 -0700

    Fixed bugs and added comments
    Signature changes list was resetting every commit instead of every repo
    Changed logic to check for added parameters instead of a difference

--- reader.py
+++ reader.py
@@ -13,49 +13,53 @@ import re
 # On 01/26/2019
 signature_pattern = "(public|protected|private|static|\s) +[\w\<\>\[\]]+\s+(\w+) *\([^\)]*\) *(\{?|[^;])"
 parameters_pattern = "\((.*?)\)"
-signature_changes = []
 
 # Get all repos from command line arguments
 repos = sys.argv[1:]
 for repo in repos:
     project_name = ""
+    signature_changes = []
     for commit in RepositoryMining(repo, only_no_merge=True,
                                    only_modifications_with_file_types=[
                                        '.java']).traverse_commits():
         project_name = commit.project_name
-        signature_changes = []
         gr = GitRepository(repo)
         for modified_file in commit.modifications:
             # if modified_file.methods:
             #     print(modified_file.methods[0].parameters)
             diff = gr.parse_diff(modified_file.diff)
-            # There can not be a change in signature
-            # if there are no additions or no deletions
+            # There can not be a change in signature if there are
+            # no additions or no deletions
             if diff['added'] and diff["deleted"]:
+                # Find the changes that were on the same line
                 line_changes = []
                 for line_added in diff['added']:
                     for line_deleted in diff['deleted']:
                         if line_added[0] == line_deleted[0]:
                             line_changes.append((line_added, line_deleted))
+
                 for line in line_changes:
                     addition = line[0][1].strip()
                     deletion = line[1][1].strip()
-                    # If found then add to signature changes
+                    # Check if the line change is a method signature
                     if re.search(signature_pattern, addition) and \
                             re.search(signature_pattern, deletion):
+                        # Get the parameters of the method signatures
                         params_new = re.search(parameters_pattern, addition)
                         params_old = re.search(parameters_pattern, deletion)
+                        # If statement is not necessary but there is a danger if
+                        # we weren't able to find the method parameters
                         if params_new and params_old:
-                            params_new = params_new.group(0).replace("(", "")\
-                                .replace(")", "").split(",")
-                            params_old = params_old.group(0).replace("(", "")\
-                                .replace(")", "").split(",")
-                            if len(params_new) != len(params_old):
+                            params_new = params_new.group(0).split(",")
+                            params_old = params_old.group(0).split(",")
+                            # If parameters were added then add to list
+                            if len(params_new) > len(params_old):
                                 signature_changes.append((commit.hash,
                                                           modified_file.filename,
                                                           deletion,
                                                           addition))
 
+    # With statement automatically closes file when finished
     with open("results/" + project_name + "_results.csv", "w") as results_file:
         csv_writer = csv.writer(results_file)
         # Add Header row to csv
