62167ac0cf325432e74eb9fad28581254610bb67
Henry Tang <hktang@ualberta.ca>
2019-01-27 17:09:29 -0700

    Modified to check parameters in order

--- reader.py
+++ reader.py
@@ -26,8 +26,6 @@ for repo in repos:
         project_name = commit.project_name
         gr = GitRepository(repo)
         for modified_file in commit.modifications:
-            # if modified_file.methods:
-            #     print(modified_file.methods[0].parameters)
             diff = gr.parse_diff(modified_file.diff)
             # There can not be a change in signature if there are
             # no additions or no deletions
@@ -52,11 +50,17 @@ for repo in repos:
                                         params_new = params_new.group(0).replace("(", "").replace(")", "").split(",")
                                         params_old = params_old.group(0).replace("(", "").replace(")", "").split(",")
                                         # If parameters were added then add to list
-                                        if len(params_new) > len(params_old) and all(params in params_new for params in params_old):
-                                            signature_changes.append((commit.hash,
-                                                                      modified_file.filename,
-                                                                      line_deleted[1].strip(),
-                                                                      line_added[1].strip()))
+                                        if len(params_new) > len(params_old):
+                                            add_line = True
+                                            for i in range(len(params_old)):
+                                                if params_new[i] != params_old[i]:
+                                                    add_line = False
+                                                    break
+                                            if add_line:
+                                                signature_changes.append((commit.hash,
+                                                                          modified_file.filename,
+                                                                          line_deleted[1].strip(),
+                                                                          line_added[1].strip()))
     print("Starting write to csv for", repo)
     # With statement automatically closes file when finished
     with open("results/" + project_name + "_results.csv", "w", newline="") \
